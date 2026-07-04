5c8a7e80457497f246fd238b26034af346a76e21
santwayne <santwayne123@gmail.com>
2026-07-04 17:23:54 +0530

    updated

--- app/database/session.py
+++ app/database/session.py
@@ -8,7 +8,14 @@ if settings.DATABASE_URL.startswith("sqlite"):
     # required for SQLite when used with FastAPI's threaded request handling
     connect_args = {"check_same_thread": False}
 
-engine = create_engine(settings.DATABASE_URL, connect_args=connect_args)
+engine = create_engine(
+    settings.DATABASE_URL,
+    connect_args=connect_args,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10,
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
